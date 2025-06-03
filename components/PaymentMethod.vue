0c957dfdc531272409d7089b8ec7e9405b552e77
wayueee <farhanhokila30@gmail.com>
2025-06-03 20:10:45 +0700

    gege

--- components/PaymentMethod.vue
+++ components/PaymentMethod.vue
@@ -9,7 +9,7 @@
     <img class="mx-auto w-[520px] mt-3" src="/images/payment/payment.webp" alt="image">
     <img class=" w-[49px] h-[52px] absolute top-[400px] left-[68px]" src="/images/payment/sparkle-left.png" alt="image">
     <img class=" w-[122px] h-[301px] absolute top-[225px] left-[1323px]" src="/images/payment/sparkle-right.png" alt="image">
-    <!-- <img class=" w-[150px] h-[246px] absolute top-[513px] left-[483px] z-20" src="/images/payment/girl-left.png" alt="image"> -->
+    <img class=" w-[150px] h-[246px] absolute top-[513px] left-[483px] z-20" src="/images/payment/girl-left.png" alt="image">
     <img class=" w-[150px] h-[171px] absolute top-[371px] left-[940px] z-20" src="/images/payment/girl-right.png" alt="image">
     <img class="w-[1192px] h-[890px] -top-[104px] absolute left-0" src="/images/payment/mid-shape.png" alt="image">
   </div>
